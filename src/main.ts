e0317b83cfa57855ee4ba45e2c9d5d11b09d841b
svatoslavarosenko <quex.modest@gmail.com>
2026-02-10 15:34:19 +0200

    Add Swagger API documentation setup and dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Mealory Core API')
+    .setDescription('API documentation')
+    .setVersion('0.0.1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('v1/api', app, document);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
